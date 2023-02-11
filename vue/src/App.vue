afc6b87210934d78f7c7d0138062212c064637d1
SergejH1990 <seho1990@gmx.de>
2023-02-11 01:19:36 +0100

    Added vue project

---
+++ vue/src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import { mapState } from 'vuex';
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  computed: {
+    ...mapState(['user'])
+  }
+}
+</script>
+
+<template>
+  <div>
+    {{ user }}
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
